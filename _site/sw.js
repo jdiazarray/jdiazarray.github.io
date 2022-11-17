b97cc9d076405405f57bed184f29f634e5d34255
jdiazaraya <juandiazaraya@gmail.com>
2022-11-17 10:39:05 -0300

    Commit prueba

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221117.102511';
+const cacheName = 'chirpy-20221117.103743';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
