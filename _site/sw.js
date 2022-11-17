045263494b35a7c05c69bc50374a3d518f5b6a2b
jdiazaraya <juandiazaraya@gmail.com>
2022-11-17 10:44:40 -0300

    commit intalacion gemas

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221117.103743';
+const cacheName = 'chirpy-20221117.104414';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
